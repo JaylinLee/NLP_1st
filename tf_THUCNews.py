20320516b36c3dbf22d008465a96237b63f9378b
Jaylin <33442971+JaylinLee@users.noreply.github.com>
2019-06-21 20:46:46 +0800

    Create tf_THUCNews.py

---
+++ tf_THUCNews.py
@@ -0,0 +1,73 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+base_dir = "C:\\Users\\APX103\\cnews"
+train_dir = os.path.join(base_dir, 'cnews.train.txt')
+test_dir = os.path.join(base_dir, 'cnews.test.txt')
+val_dir = os.path.join(base_dir, 'cnews.val.txt')
+vocab_dir = os.path.join(base_dir, 'cnews.vocab.txt')
+
+def build_vocab(train_dir, vocab_dir, vocab_size=5000):
+    """根据训练集构建词汇表，存储"""
+    data_train, _ = read_file(train_dir)  # data_train存储的是一个一个的文本
+    print(data_train)
+
+    all_data = []
+    for content in data_train:
+        all_data.extend(content)
+
+    counter = Counter(all_data)
+    count_pairs = counter.most_common(vocab_size - 1)
+    words, _ = list(zip(*count_pairs))
+    # 添加一个 <PAD> 来将所有文本pad为同一长度
+    words = ['<PAD>'] + list(words)
+
+
+open_file(vocab_dir, mode='w').write('\n'.join(words) + '\n')
+
+
+def read_file(filename):
+    """读取文件数据"""
+    contents, labels = [], []
+    with open_file(filename) as f:
+        for line in f:
+            try:
+                label, content = line.strip().split('\t')
+                if content:
+                    contents.append(list(native_content(content)))
+                    labels.append(native_content(label))
+            except:
+                pass
+    return contents, labels
+
+
+def open_file(filename, mode='r'):
+    """
+    常用文件操作，可在python2和python3间切换.
+    mode: 'r' or 'w' for read or write
+    """
+    if is_py3:
+        return open(filename, mode, encoding='utf-8', errors='ignore')
+    else:
+        return open(filename, mode)
+
+
+def read_category(categories):
+    """读取分类目录，固定"""
+    categories = ['入户盗窃', '扒窃', '一般盗窃']
+    categories = [native_content(x) for x in categories]
+
+    cat_to_id = dict(zip(categories, range(len(categories))))  # 将类别分别进行id的转换
+    
+    return categories, cat_to_id
+
+
+def read_vocab(vocab_dir):
+    """读取词汇表"""
+    with open_file(vocab_dir) as fp:
+        # 如果是py2 则每个值都转化为unicode
+        words = [native_content(_.strip()) for _ in fp.readlines()]
+    word_to_id = dict(zip(words, range(len(words))))
+    return words, word_to_id
